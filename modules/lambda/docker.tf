0704393838cd382440bc47819b56ef4d0499190e
Cristian Magherusan-Stanciu <cristi.magherusan@gmail.com>
2023-02-16 00:28:59 +0100

    Update provider dependencies
    - Major update of the kreuzwerker/docker dependency.
    - That was a breaking change which required some changes, but all in all the Docker image handling code looks much better now.

--- modules/lambda/docker.tf
+++ modules/lambda/docker.tf
@@ -1,4 +1,7 @@
-
+locals {
+  src_image = "${var.lambda_source_ecr}/${var.lambda_source_image}:${var.lambda_source_image_tag}"
+  dst_image = "${aws_ecr_repository.autospotting.repository_url}:${var.lambda_source_image_tag}"
+}
 resource "aws_ecr_repository" "autospotting" {
   name                 = "autospotting-${module.label.id}"
   image_tag_mutability = "MUTABLE"
@@ -13,11 +16,16 @@ resource "aws_ecr_repository" "autospotting" {
 
 data "aws_ecr_authorization_token" "source" {
   registry_id = split(".", var.lambda_source_ecr)[0]
-
 }
+
 data "aws_ecr_authorization_token" "destination" {}
 
 provider "docker" {
+  registry_auth {
+    address  = var.lambda_source_ecr
+    username = data.aws_ecr_authorization_token.source.user_name
+    password = data.aws_ecr_authorization_token.source.password
+  }
   registry_auth {
     address  = split("/", aws_ecr_repository.autospotting.repository_url)[0]
     username = data.aws_ecr_authorization_token.destination.user_name
@@ -25,24 +33,17 @@ provider "docker" {
   }
 }
 
-resource "local_file" "Dockerfile" {
-  content  = "FROM ${var.lambda_source_ecr}/${var.lambda_source_image}:${var.lambda_source_image_tag}"
-  filename = "${path.module}/docker/Dockerfile"
+resource "docker_image" "base_image" {
+  name = local.src_image
+}
+
+resource "docker_tag" "image" {
+  source_image = docker_image.base_image.name
+  target_image = local.dst_image
 }
 
 resource "docker_registry_image" "destination" {
-  name          = "${aws_ecr_repository.autospotting.repository_url}:${var.lambda_source_image_tag}"
+  name          = local.dst_image
   keep_remotely = false
-  build {
-    context = "${path.module}/docker"
-
-    auth_config {
-      host_name = var.lambda_source_ecr
-      user_name = data.aws_ecr_authorization_token.source.user_name
-      password  = data.aws_ecr_authorization_token.source.password
-    }
-  }
-  depends_on = [
-    local_file.Dockerfile,
-  ]
+  depends_on    = [docker_tag.image]
 }
